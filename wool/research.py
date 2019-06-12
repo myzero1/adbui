830d92eccd590e42ee7bcc7822b7f900af960827
myzero1 <myzero1@sina.com>
2019-06-12 15:03:56 +0800

    holdon

--- wool/research.py
+++ wool/research.py
@@ -1,10 +1,18 @@
 import os, sys
+import time
 file_path = os.path.dirname(sys.path[0])
 sys.path.insert(0, file_path)
 
 from adbui import Device
 
 d = Device('127.0.0.1:62001')  # 手机的sn号，如果只有一个手机可以不写
+
 d.init_ocr('10126986', 'AKIDT1Ws34B98MgtvmqRIC4oQr7CBzhEPvCL', 'AAyb3KQL5d1DE4jIMF2f6PYWJvLaeXEk')
+
 btn = d.get_ui_by_ocr(text='微信')  # 找到爱拍文字的位置
+btn.click()  # 点击爱拍
+
+time.sleep( 5 )
+
+btn = d.get_ui_by_ocr(text='登录')  # 找到爱拍文字的位置
 btn.click()  # 点击爱拍
