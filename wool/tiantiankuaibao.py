37aeb2e4e153aeda125828f4ce7d01135eab4558
myzero1 <myzero1@sina.com>
2019-06-14 17:28:54 +0800

    holdon

--- wool/tiantiankuaibao.py
+++ wool/tiantiankuaibao.py
@@ -16,5 +16,9 @@ try_num=5
 try_per_sec=12
 d = Device('127.0.0.1:62001')
 
-btn = d.get_ui_by_ocr_range_try('登录', True, (45,1149,197,1199), try_num, try_per_sec)
+# btn = d.get_ui_by_ocr_range_try('登录', True, (45,1149,197,1199), try_num, try_per_sec)
+btn = d.get_ui_by_ocr_range_try('快报', True, None, try_num, try_per_sec)
+btn.click()
+
+btn = d.get_ui_by_ocr_range_try('张大', True, None, try_num, try_per_sec)
 btn.click()
